bcecf1f3135a602d9a7e5bec0e76bc0dba8eb395
nik003kk <kaisersbergernik@gmail.com>
2025-05-13 22:39:12 +0200

    SCRUM-66 Finished component for adding crops to db

--- backend/src/models/CropModel.ts
+++ backend/src/models/CropModel.ts
@@ -1,16 +1,35 @@
-import mongoose, { Document, Schema } from "mongoose";
+import mongoose, { Document, Schema, Types } from "mongoose";
+import { ref } from "process";
 
 export interface Crop extends Document {
   name: string;
-  goodCompanions: string[];
+  latinName: string;
+  goodCompanions: Types.ObjectId[];
+  badCompanions: Types.ObjectId[];
   plantingMonth: string;
+  watering: {
+    frequency: string,
+    amount: number
+  }
 }
 
 const cropSchema = new Schema<Crop>(
   {
     name: { type: String, required: true, unique: true },
-    goodCompanions: [{type: String}],
+    latinName: { type: String, required: true, unique: true },
+    goodCompanions: [{
+      type: Schema.Types.ObjectId,
+      ref: 'Crop'
+    }],
+    badCompanions: [{
+      type: Schema.Types.ObjectId,
+      ref: 'Crop'
+    }],
     plantingMonth: {type: String, required: true},
+    watering: {
+      frequency: {type: String, required: true},
+      amount: {type: Number, required: true},
+    }
   },
   { timestamps: true }
 );
