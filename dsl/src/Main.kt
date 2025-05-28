ea056e7512086fa4b57a7e50f22329e127d94453
ozzvaa <matevz.ozvatic@gmail.com>
2025-05-28 14:08:13 +0200

    SCRUM-86 Add a few examples

--- dsl/src/Main.kt
+++ dsl/src/Main.kt
@@ -2,7 +2,10 @@
 fun main() {
     val scanner = Scanner()
 //    val tokens = scanner.readFile("magdalenskiPark.txt")
-    val tokens = scanner.readFile("mariborskiPark.txt")
+//    val tokens = scanner.readFile("mariborskiPark.txt")
+    val tokens = scanner.readFile("smesni.txt")
+//    val tokens = scanner.readFile("poljane.txt")
+
     val parser = Parser(tokens)
     parser.parse()
     parser.toGeoJson()
