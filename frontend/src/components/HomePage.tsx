8d1bdd3bd5cdcafc3375aecd233b5b8ba2c6c726
NikMori <148778780+Denomi12@users.noreply.github.com>
2025-06-05 21:49:20 +0200

    SCRUM-115 Added title to home page

--- frontend/src/components/HomePage.tsx
+++ frontend/src/components/HomePage.tsx
@@ -7,21 +7,25 @@ import styles from "../stylesheets/HomePage.module.css";
 function HomePage() {
   return (
     <div>
-      <div className={styles.title}>Welcome to home page!</div>
       <div className={styles.mainContent}>
         <div className={styles.leftSide}>
+          <div className={styles.title2}>Weather Forecast:</div>
           <div className={styles.weather}>
             <WeatherChart />
           </div>
+          <div className={styles.title2}>Map of gardens:</div>
           <div className={styles.map}>
             <Map className={styles.smallMap} showCreateButton={false} />
           </div>
         </div>
         <div className={styles.rightSide}>
+          <div className={styles.title2}>Hot Questions:</div>
           <div className={styles.hotQuestions}>
             <HotQuestion />
           </div>
           <div className={styles.calender}>
+            <div className={styles.title2}>Calender:</div>
+
             <Calender />
           </div>
         </div>
