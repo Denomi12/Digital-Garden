f68d555b5b9056bb4831f37cc130b44b66c97aa5
nik003kk <kaisersbergernik@gmail.com>
2026-01-05 11:20:18 +0100

    SCRUM-149 Data capture button

--- mobileApp/app/src/main/java/si/um/feri/mobilegarden/fragments/HomeFragment.kt
+++ mobileApp/app/src/main/java/si/um/feri/mobilegarden/fragments/HomeFragment.kt
@@ -1,10 +1,12 @@
 package si.um.feri.mobilegarden.fragments
 
+import android.graphics.Bitmap
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.navigation.fragment.findNavController
 import si.um.feri.mobilegarden.R
 import si.um.feri.mobilegarden.databinding.FragmentHomeBinding
@@ -13,6 +15,12 @@ class HomeFragment : Fragment() {
 
     private var _binding: FragmentHomeBinding? = null
     private val binding get() = _binding!!
+
+    private val takePicture = registerForActivityResult(ActivityResultContracts.TakePicturePreview()) { bitmap: Bitmap? ->
+        if (bitmap != null) {
+            binding.ivCapturedPhoto.setImageBitmap(bitmap)
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -27,6 +35,14 @@ class HomeFragment : Fragment() {
 
         binding.btnGoToExtremeEvent.setOnClickListener {
             findNavController().navigate(R.id.action_homeFragment_to_extremeEventFragment)
+        }
+
+        binding.btnCapturePhoto.setOnClickListener {
+            try {
+                takePicture.launch(null)
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
         }
     }
 
