503f750da1e2e9ef4c119af3a02e209fd9bf1e58
ozzvaa <matevz.ozvatic@gmail.com>
2026-01-18 19:07:34 +0100

    SCRUM-185 Add functioning recycler view

--- mobileApp/app/src/main/java/si/um/feri/mobilegarden/fragments/EventListFragment.kt
+++ mobileApp/app/src/main/java/si/um/feri/mobilegarden/fragments/EventListFragment.kt
@@ -1,17 +1,19 @@
 package si.um.feri.mobilegarden.fragments
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import si.um.feri.mobilegarden.databinding.FragmentEventListBinding
 import si.um.feri.mobilegarden.fileUtils.EventFileUtils
 import si.um.feri.mobilegarden.models.EventAdapter
+import si.um.feri.mobilegarden.R
 
-class EventListFragment : Fragment() {
+
+class EventListFragment : Fragment(R.layout.fragment_event_list) {
     private lateinit var binding: FragmentEventListBinding
 
     override fun onCreateView(
@@ -24,7 +26,16 @@ class EventListFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         val eventsList = EventFileUtils.loadEvents(requireContext(), "extreme_events.json")
-        val adapter = EventAdapter(eventsList)
+
+        val navController = findNavController()
+
+        val adapter = EventAdapter(eventsList) {
+            event ->
+            val bundle = Bundle().apply {
+                putString("arg_event_id", event.id)
+            }
+            navController.navigate(R.id.action_eventListFragment_to_addExtremeEventFragment, bundle)
+        }
 
         binding.rvEvents.layoutManager = LinearLayoutManager(requireContext())
         binding.rvEvents.adapter = adapter
--- mobileApp/app/src/main/java/si/um/feri/mobilegarden/models/EventAdapter.kt
+++ mobileApp/app/src/main/java/si/um/feri/mobilegarden/models/EventAdapter.kt
@@ -6,10 +6,13 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import si.um.feri.mobilegarden.databinding.EventRowItemBinding
 
-class EventAdapter(private val events: MutableList<ExtremeEvent>):
+class EventAdapter(
+    private val events: MutableList<ExtremeEvent>,
+    private val onItemClick: (ExtremeEvent) -> Unit
+) :
     RecyclerView.Adapter<EventAdapter.EventViewHolder>() {
 
-    class EventViewHolder(view: View): RecyclerView.ViewHolder(view) {
+    class EventViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         val binding = EventRowItemBinding.bind(view)
     }
 
@@ -29,7 +32,10 @@ class EventAdapter(private val events: MutableList<ExtremeEvent>):
         holder: EventViewHolder,
         position: Int
     ) {
-
+        val event = events[position]
+        holder.binding.tvName.text = event.type
+        holder.binding.tvLocation.text = event.locationName
+        holder.itemView.setOnClickListener { onItemClick(event) }
     }
 
     override fun getItemCount(): Int {
